ce92fc8f45bbed69d7691c40df7ca0fb3707ca8f
SiddhantBorude11 <siddhantsborude@gmail.com>
2025-04-27 17:11:33 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int num1,num,num3;
+cout<<"Enter number 1";
+cin>>num1;
+cout<<"Enter number 2";
+cin>>num2;
+sum = num1+num2;
+cout<<"Sum=" <<sum<<endl;
+return 0;
+}
